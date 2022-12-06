86ba833b9cd406b9e3af0380a513945a5bf81aa1
Maksim Frankov <2m.franko@gmail.com>
2022-12-06 13:44:17 +0200

    styles+hbs+firstfetch

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
+import imgCardTpl from './tamplates/imgCard.hbs';
+
+const refs = {
+    gallery: document.querySelector(".gallery")
+}
+
+
+
 const options = {
-key : '31754990-fc9ecefcb1bab85f0803676bc',
-q : 'cat',
+KEY : '31754990-fc9ecefcb1bab85f0803676bc',
+q : 'work out',
 image_type : 'photo',
 orientation :  'horizontal',
 safesearch : true,
 url : `https://pixabay.com/api/`,
 }
 
-async function fetchImg() {
-    const res = await fetch (`${options.url}?key=${options.key}&q=${options.q}&image_type=${options.image_type}&orientation=${options.orientation}&sefesearch=${options.safesearch}`);
-    console.log(res);
+function fetchImg() {
+    let res;
+     res =  fetch (`${options.url}?key=${options.KEY}&q=${options.q}&image_type=${options.image_type}&orientation=${options.orientation}&sefesearch=${options.safesearch}`)
+         .then(res => {
+             return res.json();
+         })
+         .then(images => {
+             console.log(images.hits);
+             refs.gallery.innerHTML = imgCardTpl(images.hits);
+         })
 
 };
+
+
 fetchImg()
