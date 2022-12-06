2c7657c29590872c95bd1f250790823502980794
MaksymFrankov <55510261+MaksymFrankov@users.noreply.github.com>
2022-12-06 11:44:51 +0000

    Deploying to gh-pages from @ MaksymFrankov/goit-js-hw-11@86ba833b9cd406b9e3af0380a513945a5bf81aa1 🚀

--- index.1db109db.js
+++
@@ -1,2 +0,0 @@
-const c="31754990-fc9ecefcb1bab85f0803676bc",o="cat",t="photo",a="horizontal",e=!0,n="https://pixabay.com/api/";!async function(){const i=await fetch(`${n}?key=${c}&q=${o}&image_type=${t}&orientation=${a}&sefesearch=${e}`);console.log(i)}();
-//# sourceMappingURL=index.1db109db.js.map
