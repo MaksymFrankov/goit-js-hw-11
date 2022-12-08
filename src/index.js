b07ba0e96e0bd4832cf0f801e0b2f20f604e868a
Maksim Frankov <2m.franko@gmail.com>
2022-12-08 17:15:10 +0200

    lightbox+ inf scroll + loadmore

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import imgCardTpl from './tamplates/imgCard.hbs';
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import debounce from "lodash.debounce";
+
 
 const refs = {
     gallery: document.querySelector(".gallery"),
@@ -11,17 +13,15 @@ const refs = {
 }
 
 const options = {
-    KEY : '31754990-fc9ecefcb1bab85f0803676bc',
-    PER_PAGE: 40,
-    DEBOUNCE_DELAY: 300,
+    key : '31754990-fc9ecefcb1bab85f0803676bc',
+    per_page: 40,
     q : '',
     image_type : 'photo',
     orientation :  'horizontal',
     safesearch : true,
-    url: `https://pixabay.com/api/`,
-    page: 1,
-    
+    page: 0,
 }
+const host = `https://pixabay.com/api/`;
 
 refs.searchBtn.addEventListener('click' ,onSearchBtnClick);
 refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
@@ -30,32 +30,67 @@ refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
 function onSearchBtnClick(e) {
     e.preventDefault();
     options.q = refs.inputedQ.value;
+    refs.gallery.innerHTML = '';
+    options.page = 0;
     fetchImg();
     toggleHidden();
 }
 
 function onLoadMoreBtnClick() {
-    options.page++;
     fetchImg();
 }
 
+
+
 function fetchImg() {
+    options.page++;
     let res;
-     res =  fetch (`${options.url}?key=${options.KEY}&q=${options.q}&image_type=${options.image_type}&orientation=${options.orientation}&sefesearch=${options.safesearch}&page=${options.page}&per_page=${options.PER_PAGE}`)
-         .then(res => {
+    /*
+    `${options.url}?key=${options.KEY}&q=${options.q}&image_type=${options.image_type}&orientation=${options.orientation}&sefesearch=${options.safesearch}&page=${options.page}&per_page=${options.PER_PAGE}`
+    */
+    let url = [];
+    for (const param in options)
+    {
+        url.push(`${param}=${options[param]}`);
+    }
+    res = fetch(`${host}?${url.join('&')}`)
+        .then(res => {
             return res.json();
-         })
-         .then(images => {
-            console.log(images);
-             if (images.hits.length > 0) {
-                 refs.gallery.innerHTML = imgCardTpl(images.hits);
-             }
-             else  {
-                  Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-             }
-         })
+        })
+        .then(res => {
+            renderGallery(res);
+            return res;
+        })
+        .then(res => {
+            if (options.page == 1)
+                Notiflix.Notify.info(`Hooray! We found ${res.totalHits} images.`)
+        })
+        .catch(err => onFetchError(err))
 
 };
+
+function renderGallery(images)
+{
+    console.log(images)
+    if (images.hits.length > 0) {
+        refs.gallery.innerHTML += imgCardTpl(images.hits);
+    }
+    else  {
+         Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+    }
+
+    let gallery = new SimpleLightbox(".gallery a", {
+        captionsData: "alt",
+        captionsDelay: 250,
+        doubleTapZoom: 1,
+        scrollZoom: false
+    });
+
+    gallery.on('shown.simplelightbox', function () {
+        let el = document.querySelector(".sl-image");
+        el.addEventListener('click', (el) => gallery.close(el));
+    });
+}
 
 function toggleHidden() {
     refs.loadMoreBtn.classList.toggle("hidden");
@@ -64,7 +99,21 @@ function toggleHidden() {
 }
 
 function onFetchError(err) {
-    console.log(err);
+    throttleFun();
 }
-// скролл + прокрутка + лайтбокс 
-// После первого запроса при каждом новом поиске выводить уведомление в котором будет написано сколько всего нашли изображений (свойство totalHits). Текст уведомления "Hooray! We found totalHits images."
+
+const throttleFun = debounce(() => { Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.") }, 3000, { 'maxWait': 0, 'leading': true, 'trailing':false});
+
+const handleInfinityScroll = () => {
+    const EndOfPage = window.innerHeight + window.scrollY >= document.body.offsetHeight;
+
+    if (EndOfPage) {
+        fetchImg();
+    }
+}
+
+window.addEventListener("scroll", handleInfinityScroll);
+
+
+
+
